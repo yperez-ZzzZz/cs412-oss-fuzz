a7741c2dc9348d60774285ed052106fa27455bd2
Agathe Claire Denyse Jeanne Pelayo <ariel.pelayo@epfl.ch>
2025-05-12 23:37:06 +0200

    report with corpus + coverage commands to be deleted

--- part1/run.w_corpus.sh
+++ part1/run.w_corpus.sh
@@ -4,4 +4,8 @@ python3 infra/helper.py build_fuzzers libpng
 
 # store results to build/out/corpus
 mkdir build /out/corpus/
-python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus
+python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus/
+
+# generate coverage
+python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
+python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus/ --fuzz-target libpng_read_fuzzer
--- part1/run.w_o_corpus.sh
+++ part1/run.w_o_corpus.sh
@@ -3,5 +3,9 @@ python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 
 # store results to build/out/corpus
-mkdir build /out/corpus/
-python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus
+mkdir build /out/corpus_w_o/
+python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus_w_o/
+
+# generate coverage
+python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
+python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus_w_o/ --fuzz-target libpng_read_fuzzer
