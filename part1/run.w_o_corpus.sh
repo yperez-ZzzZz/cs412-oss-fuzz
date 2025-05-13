f0419df223c74c28640996eac2e967689738a9e5
Agathe Claire Denyse Jeanne Pelayo <ariel.pelayo@epfl.ch>
2025-05-13 17:21:20 +0200

    correct use of git clone :clown_emoji:

--- part1/run.w_o_corpus.sh
+++ part1/run.w_o_corpus.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/yperez-ZzzZz/cs412-oss-fuzz/tree/w_o_corpus && cd cs412-oss-fuzz/oss-fuzz
+git clone -b w_o_corpus https://github.com/yperez-ZzzZz/cs412-oss-fuzz.git && cd cs412-oss-fuzz/oss-fuzz
 python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 
 # store results to build/out/corpus
-mkdir build /out/corpus_w_o/
+mkdir build/out/corpus_w_o/
 python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus_w_o/
 
 # generate coverage
