ac76317329be5b5073488a68333545ae0c75886e
Agathe Claire Denyse Jeanne Pelayo <ariel.pelayo@epfl.ch>
2025-05-14 14:06:40 +0200

    run fuzzer with and without seeds using our forks

--- part1/run.w_corpus.sh
+++ part1/run.w_corpus.sh
@@ -1,11 +1,7 @@
-git clone https://github.com/google/oss-fuzz && cd oss-fuzz
-python3 infra/helper.py build_image libpng
+git clone https://github.com/yperez-ZzzZz/cs412-oss-fuzz.git && cd cs412-oss-fuzz/oss-fuzz
+yes | python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 
-# store results to build/out/corpus
-mkdir build /out/corpus/
+# store results to build/out/corpus/
+mkdir build/out/corpus/
 python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus/
-
-# generate coverage
-python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
-python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus/ --fuzz-target libpng_read_fuzzer
--- part1/run.w_o_corpus.sh
+++ part1/run.w_o_corpus.sh
@@ -1,11 +1,8 @@
-git clone -b w_o_corpus https://github.com/yperez-ZzzZz/cs412-oss-fuzz.git && cd cs412-oss-fuzz/oss-fuzz
-python3 infra/helper.py build_image libpng
+git clone -b w_o_corpus https://github.com/yperez-ZzzZz/cs412-oss-fuzz.git
+cd cs412-oss-fuzz/oss-fuzz
+yes | python3 infra/helper.py build_image libpng
 python3 infra/helper.py build_fuzzers libpng
 
-# store results to build/out/corpus
+# store results to build/out/corpus_w_o/
 mkdir build/out/corpus_w_o/
 python3 infra/helper.py run_fuzzer libpng libpng_read_fuzzer --corpus-dir build/out/corpus_w_o/
-
-# generate coverage
-python3 infra/helper.py build_fuzzers --sanitizer coverage libpng
-python3 infra/helper.py coverage libpng --corpus-dir build/out/corpus_w_o/ --fuzz-target libpng_read_fuzzer
